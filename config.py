9a8db42f66a1ce184ef1e6475924141a2c018940
_eternal_flame <webmaster@andycloud.dynu.net>
2018-04-21 14:37:44 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# mail
+imap_host = "imap.example.com"
+smtp_host = "smtp.example.cpm"
+mail_username = "username"
+sender = "myself@example.com"
+mail_passwd = r"password"
+
+# senders
+valid_sender = ("mymail@example.com",
+                "anothermail@example.com",
+                )
+valid_prefix = "fwthisfile"
+
+# output
+zipname = "myzip.zip"
+outsubject = "my_attachment"
+out_email_address = "sendhere@example.com"
